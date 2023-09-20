f92a6e7300b1efe096f0cff6c091c75abf726869
Chinecherem <chinecheremubawike@gmail.com>
2023-09-20 17:51:47 +0100

    Implement a feature that allows firebase to fetch data in real time

--- src/Routes/admin/Courses/Courses.jsx
+++ src/Routes/admin/Courses/Courses.jsx
@@ -3,14 +3,13 @@ import { Fragment, useState } from 'react';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
 import {firestore, storage} from '../../../utils/firebase.utils';
 
 import { addCourse } from '../../../store/courses/courses.actions';
 import Navbar from '../../../components/general/Navbar/Navbar';
 import Sidebar from '../../../components/general/Sidebar/Sidebar';
 import CoursesCard from '../../../components/admin/courseCard/CoursesCard';
-
 
 
 export default function Courses() {
@@ -23,33 +22,46 @@ export default function Courses() {
   const handleShow = () => setShowOffcanvas(true);
   const handleClose = () => setShowOffcanvas(false);
 
+  // useEffect(() => {
+  //   // Fetch all documents in the collection
+  //   getDocs(collectionRef)
+  //   .then((querySnapshot) => {
+  //     const newCoursesData = [];
+  //     // Iterate through the documents
+  //     querySnapshot.forEach((doc) => {
+  //       // Document data is in doc.data()
+  //       const courseDocumentData = doc.data();
+  //       console.log('Document Data:', courseDocumentData);
+  //       // dispatch(addCourse({
+  //       //     [courseDocumentData.courseName] : {
+  //       //       courseName : courseDocumentData.courseName
+  //       //     }
+  //       // }))
+  //       newCoursesData.push(courseDocumentData);
+  //     });
+  //     // Update the state after fetching the data
+  //     setCoursesData(newCoursesData);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error getting documents:', error);
+  //   });
+  // }, [])
   useEffect(() => {
-    console.log('hello')
-    // Fetch all documents in the collection
-    getDocs(collectionRef)
-    .then((querySnapshot) => {
+    // Create a real-time listener using onSnapshot
+    const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
       const newCoursesData = [];
-      // Iterate through the documents
+
       querySnapshot.forEach((doc) => {
-        // Document data is in doc.data()
         const courseDocumentData = doc.data();
-        console.log('Document Data:', courseDocumentData);
-        // dispatch(addCourse({
-        //     [courseDocumentData.courseName] : {
-        //       courseName : courseDocumentData.courseName
-        //     }
-        // }))
         newCoursesData.push(courseDocumentData);
       });
-      // Update the state after fetching the data
-      console.log(newCoursesData);
+
       setCoursesData(newCoursesData);
-      console.log(coursesData);
-    })
-    .catch((error) => {
-      console.error('Error getting documents:', error);
     });
-  }, [])
+
+    // Return a cleanup function to unsubscribe from the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
   return (
     <Fragment>
         <Navbar handleShow={handleShow} />
